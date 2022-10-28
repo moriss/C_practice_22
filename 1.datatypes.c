122d96f1fda365a6d60d188914bcf967b652602a
moriss <marrocco001@gmail.com>
2022-10-29 00:18:10 +0100

    Update 1.datatypes.c

--- 1.datatypes.c
+++ 1.datatypes.c
@@ -8,46 +8,34 @@ long 	8 bites // To 4 billion and beyond
 string  An indeterminate number of bytes since it can be a whole book
 
 
-//Types, formats, operators
-
+//Types, formats, 
 //And the CS50 library has corresponding functions to get input of various types:
 // Its best to declare a variable just when you need it (scope).
-
-// get_char  : only 1 byte of memory for individual characters.
-
-// get_float  :real or decimal numbers contained in 4 bytes of memory
-
-// get_double, For storing floating point numbers. Double precision compared to floats. So: are contained in 8 bytes of data.
-
-//get_int : 4 bytes of memory or 32 bits, 50% pos numbs, 50% negatives
-// -2 to the 31st and 2 to the 31st - 1
+get_char  //: only 1 byte of memory for individual characters.
+get_float  //:real (decimal) numbers contained in 4 bytes of memory
+get_double // For storing floating point numbers. Double precision compared to floats. So: are contained in 8 bytes of data.
+get_int //: 4 bytes of memory or 32 bits, 50% pos numbs, 50% negatives -2 to the 31st and 2 to the 31st - 1
 // range= approx negative 2 billion to positive 2 billion
-			
 // unsigned int : Only allows positive numbers thus doubling positive's range to approx 4 billion 
-// unsigned int a = 3;
 
-// Others include: short, long and const
-
-
-// #########################
-//void, a type but not a data type. For functions that don't return a value eg printf. The Result is not caputered and stored,  just printed on screen 
+ void //, a type but not a data type. For functions that don't return a value eg printf. The Result is not caputered and stored,  just printed on screen 
 //as a side event. Therefore, printf is a void function.
 
 // void my(void)  : parameters can also be void. EG: no arguments get passed into main.
-
-// bool : in cs50 library. For true or false. Standard data type in most all langauges expect for C
 
 //############################
 // get_string, is from the cs59 library
 //############################
 
+//Placeholders
 // For printf, too, there are different placeholders for each type:
-// %c for chars
-// %f for floats, doubles
-// %i for ints
-// %li for longs
-// %s for strings
+%c //for chars
+%f //for floats, doubles
+%i // for ints
+%li // for longs
+%s // for strings
 
+//operators
 // And there are some mathematical operators we can use:
 // == For is eqaul to. AKA the equality operator. A single = is the assignment operator. (copies from right into left)
 // + for addition
