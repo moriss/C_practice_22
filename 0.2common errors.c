0308a7412ab093bb375daa89df730b744e64ace0
moriss <marrocco001@gmail.com>
2022-10-31 10:37:00 +0000

    Update 0.2common errors.c

--- 0.2common errors.c
+++ 0.2common errors.c
@@ -1,2 +1,6 @@
 //Ommiting a coma in printf between first string input and second input.
   printf("%i n is greater than 3 \n" , n );
+
+//When typing "make <filename> into comand line I got this:
+make: *** No rule to make target 'first'.  Stop.
+//Possible Reason: The file isn't at this location. Use ls in command line to navigate to correct folder
