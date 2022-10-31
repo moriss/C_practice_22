8eff549d576e5e38660a9925681b8ec6a9a9ef96
moriss <marrocco001@gmail.com>
2022-10-31 11:53:35 +0000

    Update 0.2common errors.c

--- 0.2common errors.c
+++ 0.2common errors.c
@@ -4,3 +4,7 @@
 //When typing "make <filename> into comand line I got this:
 make: *** No rule to make target 'first'.  Stop.
 //Possible Reason: The file isn't at this location. Use ls in command line to navigate to correct folder
+  
+
+// Less than or equal to
+  while (i <= 4) : /*It must be done in this order*/. while(i =<) //doesn't work.
