1904c807fbcc8615f73c7305372481d9c7bf1bdb
moriss <marrocco001@gmail.com>
2022-11-24 12:38:53 +0000

    Update 0.2_errors.c

--- 0.2_errors.c
+++ 0.2_errors.c
@@ -1,3 +1,27 @@
+###############
+// error: implicit declaration of function 'my' is invalid in C99 
+//I did not firstly explicitly declare the function
+//It should be explicitly declared here   void my(void);
+int main(void)
+{
+    my();
+}
+
+void my(void)
+{
+
+
+    int num1 = 6;
+    int num2 = 10;
+    int num3 = N;
+
+    printf("%i\n %i\n %i\n " , num1 + N , num2 + N , num3 + N);
+}
+//If a name appears in a program and is not explicitly declared, it is implicitly declared
+###############
+
+
+
 //error: expected function body after function declarator. Reason: forgot curly braces right after:
 void my(void){  }
 
