e6f3932934bef390ab697f44b8a6fbf5021b3927
moriss <marrocco001@gmail.com>
2022-12-10 08:12:34 +0000

    Create 0.21_getAwayWith.c

---
+++ 0.21_getAwayWith.c
@@ -0,0 +1,7 @@
+//Normally, printf requires curly braces (after the for loop) but seems to work fine like this. Ie, No Errors!
+
+void my(int x)
+{
+    for(int i = 0; i < x; i ++)
+        printf("God \n");
+}
