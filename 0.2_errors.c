bbbb0266ab93f4b2de901b8eb9372862770d703a
moriss <marrocco001@gmail.com>
2023-01-14 09:12:48 +0000

    Update 0.2_errors.c

--- 0.2_errors.c
+++ 0.2_errors.c
@@ -1,16 +1,16 @@
 ###############
 // error: implicit declaration of function 'my' is invalid in C99 
-//I did not firstly explicitly declare the function
-//It should be explicitly declared here   void my(void);
+//I did not firstly explicitly declare the function: void my(void);
+//It should be explicitly declared here  (above 'main') :
+
+const int N=3;//I just thought I would throw in a curve ball 'const'  
 int main(void)
 {
-    my();
+    my();//Run my function in main
 }
 
 void my(void)
 {
-
-
     int num1 = 6;
     int num2 = 10;
     int num3 = N;
@@ -22,16 +22,25 @@ void my(void)
 
 
 
-//error: expected function body after function declarator. Reason: forgot curly braces right after:
-void my(void){  }
+//error: expected function body after function declarator. Reason: forgot curly braces right after void my(void): Fairly self explanatory.
+void my(void)
+    my();
+
+
 
 //Ommiting a coma in printf between first string input and second input.
-  printf("%i n is greater than 3 \n" , n );
+  printf("%i is greater than 3 \n"   /**coma goes here**/  N );//The placeholder is for the const N.
+
+
+
 
 //When typing "make <filename> into comand line I got this:
 make: *** No rule to make target 'first'.  Stop.
-//Possible Reason: The file isn't at this location. Use ls in command line to navigate to correct folder
+//Possible Reason: The file isn't at this location. Use ls in command line to check it's there.
   
+    
+    
 
 // Less than or equal to
-  while (i <= 4) : /*It must be done in this order*/. while(i =<) //doesn't work.
+    error: expected expression
+  while (i => 4) : // It must be done in the following order  while(i <=) , otherwise it doesn't work.
