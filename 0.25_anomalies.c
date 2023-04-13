d4fb116fa36cf05568d73c48dee54f2630d61499
moriss <marrocco001@gmail.com>
2023-04-13 21:46:43 +0100

    Delete 0.25_anomalies.c

--- 0.25_anomalies.c
+++
@@ -1,9 +0,0 @@
-//Normally, printf requires curly braces (after the for loop) but seems to work fine like this. Ie, No Errors!
-
-void my(int x)
-{
-    for(int i = 0; i < x; i ++)
-        //No Curly Braces Here
-        printf("God \n");
-        //No Curly Braces Here
-}
