acf46002fa897ceae742e2f88c14f0a75de1d242
moriss <marrocco001@gmail.com>
2023-04-15 08:39:55 +0100

    Delete 0.4 Include Headers.c

--- 0.4 Include Headers.c
+++
@@ -1,7 +0,0 @@
-#include <cs50.h>//Contains get_int, get_string
-#include <stdio.h> //Contains printf
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <math.h>
