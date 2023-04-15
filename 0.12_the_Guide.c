4efdbdc131da4362e0c7dcbf4996ffcc414f579b
moriss <marrocco001@gmail.com>
2023-04-15 09:02:46 +0100

    Update 0.12_the_Guide.c

--- 0.12_the_Guide.c
+++ 0.12_the_Guide.c
@@ -1,6 +1,11 @@
 //Contents
 //Some Terms , Anomalies , Const , Parity ,  User input 1  ,  While loop 1  ,  For loop one  ,  Own Function one
 
+// Include Headers.c  ,   operators ,  Datatypes  ,  Formats for datatypes  ,   Placeholders  ,  
+
+
+
+
 //Some Terms
 //Modularising our code
 //He said this when he created his own function. 
@@ -128,3 +133,75 @@ int main(void)
     meeawo();
 }
 //Above is just to show it can be done this way. By convention, main sits at the top with your programm's prototype at the top and the detail down below somewhere.
+    
+    
+
+    
+// Include Headers.c
+#include <cs50.h>//Contains get_int, get_string
+#include <stdio.h> //Contains printf
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+    
+    
+    
+    
+//  operators
+// And there are some mathematical operators we can use:
+// == For is eqaul to. AKA the equality operator. A single = is the assignment operator. (copies from right into left)
+// + for addition
+// - for subtraction
+// * for multiplication
+// / for division
+// % for remainder (modulu) The remainder when two numbers are divided together.
+// || The or operator
+// && The and operator
+
+
+    
+    
+// Datatypes
+bool	1 bite  // EG:if(x > y), is boolean since it can only be either yes or no or true or false
+char	1 bite //  A single character like 'a' or 't' (Note that single quotes are used for chars).
+int	4 bites // roughly up to 2 billion (taking into account negative numbers)
+float	4 bites // A floating-point value, or real number with a decimal value
+double	8 bites // A floating-point value roughly double in size of a float.
+long 	8 bites // To 4 billion and beyond. Use this for more precision
+string  //An indeterminate number of bytes since it can be a whole book
+void //, a type but not a data type. For functions that don't return a value eg printf. The Result is not captured and stored,  just printed on screen 
+//(as a side event). Therefore, printf is a void function.
+
+// void my(void)  : parameters can also be void. EG: no arguments get passed into main.
+    
+    
+    
+    
+//  Formats for datatypes 
+//And the CS50 library has corresponding functions to get input of various types:
+// Its best to declare a variable just when you need it (scope).
+get_string //:Frome the cs50 library
+get_char  //: only 1 byte of memory for individual characters.
+get_float  //:real (decimal) numbers contained in 4 bytes of memory
+get_double // For storing floating point numbers. Double precision compared to floats. So: are contained in 8 bytes of data.
+get_int //: 4 bytes of memory or 32 bits, 50% pos numbs, 50% negatives -2 to the 31st and 2 to the 31st - 1
+// range= approx negative 2 billion to positive 2 billion
+// unsigned int : Only allows positive numbers thus doubling positive's range to approx 4 billion 
+
+ 
+    
+    
+    
+//  Placeholders
+// For printf, too, there are different placeholders for each type:
+%c //for chars
+%f //for floats, doubles  NB %.2f Returns 2 decimal places
+%i // for ints
+%li // for longs
+%s // for strings
+    
+    
+    
+    
