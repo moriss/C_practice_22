be6b7c71b442f1c2becf43daec0f41522651f223
moriss <marrocco001@gmail.com>
2023-04-22 09:46:37 +0100

    Update 5_Block_of_hashes.c

--- 5_Block_of_hashes.c
+++ 5_Block_of_hashes.c
@@ -11,3 +11,49 @@ int main(void)
             printf("\n");//Line break occurs at end of fourth hash.
     }
 }
+
+
+
+
+
+//print a block of hashes, asking the user for the size.
+int main(void)
+{
+    int n = get_int("How large do you want the block? :");
+
+    for(int i = 0; i < n; i ++)//Outer loop that runs while i is less or equal to 3
+    {
+
+        for(int j = 0; j < n; j ++)//Inner loop that prints the lines, again less or equal to 3
+        {
+            printf("# ");//Printing the hashes
+        }
+            printf("\n");//Line break occurs at end of last hash.
+    }
+}
+
+
+
+
+//print a block of hashes, asking the user for the size.
+//Re-ask, if they entered an invalid number.
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("How large do you want the block? :");
+    }
+
+    while (n < 1);
+
+    for(int i = 0; i < n; i ++)//Outer loop that runs while i is less or equal to 3
+    {
+
+        for(int j = 0; j < n; j ++)//Inner loop that prints the lines, again less or equal to 3
+        {
+            printf("# ");//Printing the hashes
+        }
+            printf("\n");//Line break occurs at end of last hash.
+    }
+}
