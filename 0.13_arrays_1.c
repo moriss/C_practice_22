30a7ee5655c2c213936db229680b62a3f0cb3c82
moriss <marrocco001@gmail.com>
2023-06-28 11:04:22 +0100

    Update 0.13_arrays_1.c

--- 0.13_arrays_1.c
+++ 0.13_arrays_1.c
@@ -8,3 +8,13 @@ int main(void)
 	
 	printf("Average = %i\n ", (scores [0] + scores [1] + scores [2]) / 3);
 }
+
+//This time ask for user input
+	int scores[3];
+	scores [0] = get_int("first score = ");
+	scores [1] = get_int("second score = ");
+	scores [2] = get_int("third score = ");
+	
+	printf("Average = %i\n ", (scores [0] + scores [1] + scores [2]) / 3);
+
+
