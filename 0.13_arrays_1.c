c17b8243f65c531a5f65935f9d3a30431f81e3e9
moriss <marrocco001@gmail.com>
2023-06-29 13:59:02 +0100

    Update 0.13_arrays_1.c

--- 0.13_arrays_1.c
+++ 0.13_arrays_1.c
@@ -1,3 +1,5 @@
+//A gentle introduction to arrays.
+
 //Very basic use of an array to print out 3 hard coded integers to be averaged.
 int main(void)
 {
@@ -18,3 +20,14 @@ int main(void)
 	printf("Average = %i\n ", (scores [0] + scores [1] + scores [2]) / 3);
 
 
+//Arrays and for loops
+int main(void)
+{
+      int n = get_int("Number of Assignments? ");
+      int assignments[n];
+      for(int i = 0; i < n; i ++)
+      {
+            printf("%i ", assignments[i]);//This seems to print out four random numbers which may be addressees?
+      }
+      
+}
