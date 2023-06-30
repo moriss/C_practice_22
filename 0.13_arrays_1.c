bb5bbf1f3825a45ce5c801956ed9ff1f7b6b0594
moriss <marrocco001@gmail.com>
2023-06-30 21:22:25 +0100

    Update 0.13_arrays_1.c

--- 0.13_arrays_1.c
+++ 0.13_arrays_1.c
@@ -1,4 +1,20 @@
 //A gentle introduction to arrays.
+//Arrays and for loops
+int main(void)
+{
+      int n = get_int("Number of Assignments? ");
+      int assignments[n];
+      for(int i = 0; i < n; i ++)
+      {
+            printf("%i ", assignments[i]);//This prints out garbage data! Explanation below
+      }
+      /**The i in the assignments array represents the location of each element in the array depending on where you are in the for loop.
+      In other words, the i in the for loop matches the indexed postion of the array's elements. 
+      So the first i in the for loop points to the first element in the array and so on. 
+      As you loop through each i in the for loop, this same i points to the corresponding element in the array. 
+      The contents of each memory location is just garbage leftover data. **/
+}
+
 
 //Very basic use of an array to print out 3 hard coded integers to be averaged.
 int main(void)
@@ -20,14 +36,3 @@ int main(void)
 	printf("Average = %i\n ", (scores [0] + scores [1] + scores [2]) / 3);
 
 
-//Arrays and for loops
-int main(void)
-{
-      int n = get_int("Number of Assignments? ");
-      int assignments[n];
-      for(int i = 0; i < n; i ++)
-      {
-            printf("%i ", assignments[i]);//This seems to print out four random numbers which may be addressees?
-      }
-      
-}
