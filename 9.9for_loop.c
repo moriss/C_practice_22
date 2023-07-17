a6222c58650ce7b96f4b01ba72a5f51fa556562f
moriss <marrocco001@gmail.com>
2023-07-17 16:02:20 +0100

    Update 9.9for_loop.c

--- 9.9for_loop.c
+++ 9.9for_loop.c
@@ -1,1 +1,19 @@
 
+//Count number of chars in user's string using a for loop
+int my(char*x);
+int main(void)
+{
+     char * name = get_string("Name: ");
+     int a = my(name);
+     printf("%i ", a);
+}
+
+int my(char*x)
+{
+     int add=0;
+     for(int i = 0; x[i] != '\0'; i++)
+     {
+          add++;
+     }
+     return add;
+}
