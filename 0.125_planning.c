0a020134337908c564aff84fd48828de2221172d
moriss <marrocco001@gmail.com>
2023-07-25 13:02:18 +0100

    Update 0.125_planning.c

--- 0.125_planning.c
+++ 0.125_planning.c
@@ -1,18 +1,21 @@
-/**Planning and carpet coding
+Contents: 
+/** Introduction
+  A Guide 
+  Change chars all to uppercase
+  Counting Chars
+  Change chars all to uppercase
+**/
+
+/**Introduction
 This file catapults any tasks from description to planning to implementation. Implementatioin requires that tasks are performed in different ways then added to the relevant files. 
 Different ways include using different loops (for the same task) and other approaches to be added as you learn them. 
 
 So we begin with a description, then a mental planning, then writing the code. 
 It is assumed you always write your own function and where possible return a value and save it in a variable to be used later.
 
-Contents: 
-/** 
-  counting chars 
-  Change chars all to uppercase
-  
-**/
 
-//A guide:
+
+//A Guide:
 //In main, start by working out what you need from the user then using this as input to your -yet to be created -function. 
 //Remember to use the correct datatypes and consider if your function will have an output to be saved into a variable.
 //Now shift down to your own function-writing area representing the user's input with a placeholder
@@ -24,8 +27,8 @@ Contents:
     //Try different ways to write the code ie for, while and do while loops then save what you think is the best way below.
     **/
 
-carpet code practice
-// counting chars 
+
+// Counting Chars 
 //We can start with counting the lentgh of a users string input and placing the result in a variable.
 /**
 1. Ask user for a string input.
@@ -35,7 +38,7 @@ carpet code practice
 5. By secondly using your own function and calling it in main.
 6. By thirdly using an existing library function.
 7. Make sure to try in all loop functions (for, while and do while) 
-Only save the latest below
+Only save below the one that you think is the best approach.
 **/
   {
      char * name = get_string("Name: ");
