b01ed53d3b5fe3a3baef0123451ed90af296c942
moriss <marrocco001@gmail.com>
2023-07-25 15:15:43 +0100

    Update 7.while_loop_option.c

--- 7.while_loop_option.c
+++ 7.while_loop_option.c
@@ -3,6 +3,31 @@
   Count the characters of a user's string input using a while loop
 ***/
 
+int main(void)
+{
+    char * name = get_string("Name: " );
+    my(name);
+}
+
+void my(char* x)
+{
+    int i=0;
+    int a = strlen(x);
+    while (i < a)
+    {
+        x[i] = toupper(x[i]);
+        printf("%c ", x[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
 // Count the characters of a user's string input using a while loop
 int my(char*x);
 int main(void)
