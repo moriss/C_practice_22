db18dc61528eb8d82b978bd2b190cbfd2ef87699
moriss <marrocco001@gmail.com>
2023-07-27 14:40:13 +0100

    Update 9.8_Library_functions.c

--- 9.8_Library_functions.c
+++ 9.8_Library_functions.c
@@ -1,3 +1,8 @@
+/**Contents
+Use the string length (strlen library function) to save and print the string length of user's input
+Use library functions to convert any lowercase letters to uppercase
+**/
+
 #include <string.h>
 //Use the string length (strlen library function) to save and print the string length of user's input
 int my(char*x);
@@ -6,4 +11,26 @@ int main(void)
      char * name = get_string("Name: ");
      int a = strlen(name);
      printf("%i ", a);
+}
+
+
+
+
+//Use library functions to convert any lowercase letters to uppercase
+void my(char*x)
+{
+    for(int i = 0, length=strlen(x) ; i<length; i ++)
+    {
+        if (isupper(x[i]))
+        {
+            printf("%c ", x[i]);
+        }
+
+        else
+        {
+            x[i] = toupper(x[i]);
+            printf("%c ", x[i]);
+        }
+
+    }
 }
