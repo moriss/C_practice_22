d4ce866ecee4ba6e2b43eadf0ec2323c66361c8a
moriss <marrocco001@gmail.com>
2023-07-27 13:57:27 +0100

    Update 9.9_for_loop_option.c

--- 9.9_for_loop_option.c
+++ 9.9_for_loop_option.c
@@ -7,11 +7,15 @@ Count number of chars in user's string using a for loop
 
 
 // Converting user's string to all uppercase using a for loop
-void my(char* s)
+void my(char*x)
 {
-    for(int i = 0 ,  n=strlen(s) ; i < n ; i ++ )
+    for(int i = 0, length = strlen(x); i<length; i ++)
     {
-        printf("%i ", toupper(s[i]));
+        if (x[i] >= 'a' && x[i] <= 'z')//integers can also be used here (97 and 122 respectviely)
+        {
+            x[i] = x[i] -32;// Minusing 32 from small letters outputs capital letters.
+        }
+        printf("%c ", x[i]);
     }
 }
 
